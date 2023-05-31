93c3a8069419725dd838b10900d20eeb0eb69c57
Sanjay Yadav <sanjayyadav@Sanjays-MacBook-Pro.local>
2023-05-31 21:04:33 +0530

    added more tests

--- test/mocks/PoolUtilsMockForDepositFlow.sol
+++ test/mocks/PoolUtilsMockForDepositFlow.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.16;
 
+import '../../contracts/StaderConfig.sol';
+
 contract PoolUtilsMockForDepositFlow {
     address nodeRegistry;
+    address staderConfig;
 
     error EmptyNameString();
     error NameCrossedMaxLength();
@@ -13,20 +16,20 @@ contract PoolUtilsMockForDepositFlow {
     uint64 private constant PUBKEY_LENGTH = 48;
     uint64 private constant SIGNATURE_LENGTH = 96;
 
-    constructor(address _nodeRegistry) {
+    constructor(address _nodeRegistry, address _staderConfig) {
         nodeRegistry = _nodeRegistry;
-    }
-
-    function getOperatorPoolId(address) external pure returns (uint8) {
-        return 1;
+        staderConfig = _staderConfig;
     }
 
     function getNodeRegistry(uint8) public view returns (address) {
         return nodeRegistry;
     }
 
-    function getCollateralETH(uint8) public pure returns (uint256) {
-        return 4 ether;
+    function getCollateralETH(uint8 poolID) public pure returns (uint256) {
+        if (poolID == 1) {
+            return 4 ether;
+        }
+        return 0;
     }
 
     function isExistingPubkey(bytes calldata) public pure returns (bool) {
@@ -35,6 +38,13 @@ contract PoolUtilsMockForDepositFlow {
 
     function isExistingOperator(address) external pure returns (bool) {
         return false;
+    }
+
+    function poolAddressById(uint8 poolID) external view returns (address) {
+        if (poolID == 1) {
+            return StaderConfig(staderConfig).getPermissionlessPool();
+        }
+        return StaderConfig(staderConfig).getPermissionedPool();
     }
 
     function onlyValidName(string calldata _name) external pure {
